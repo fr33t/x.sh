c2e3103bb8fcaa9d92425bce56aab8c7c15db3e2
fb0sh <fb0sh@outlook.com>
2024-08-11 10:54:40 +0800

    Create x.sh

---
+++ x.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# author: fb0sh@outlook.com
+file_path=$1
+line="###############################"
+
+# file
+_file() {
+        echo "$line File Type $line"
+        echo "$(file $file_path | awk -F: '{print $1 ": \x1b[32m" $2 "\x1b[0m"}')"
+        echo -e "\n"
+}
+
+# strings
+_strings() {
+        echo "$line Strings-Default $line"
+        echo "$(strings -a $file_path)"
+
+        echo "$line Strings-Flag $line"
+        echo "$(strings $file_path | grep "{.*}")"
+
+        echo "$line Strings-Base_encode $line"
+        echo "$(strings $file_path | grep "[A-Za-z0-9/+=]|[A-Z2-7=]|[A-F0-9=]")"
+
+        echo -e "\n"
+
+}
+
+# hexdump
+_hexdump() {
+        echo "$line Hex Dump $line"
+        echo "$(hexdump -C $file_path)"
+        echo -e "\n"
+
+}
+
+# exiftool
+_exiftool() {
+        echo "$line Exiftool $line"
+        echo "$(exiftool $file_path | sed -e 's/^XP Comment.*/\x1b[32m&\x1b[0m/')"
+        echo -e "\n"
+}
+
+# binwalk & foremost
+_extract() {
+        echo "$line Binwalk $line"
+        out_dir="_binwalk.$file_path"
+        echo "$(binwalk -e $file_path -C $out_dir)"
+
+        echo "$line Foremost $line"
+        out_dir="_foremost.$file_path"
+        echo "$(foremost -i $file_path -o $out_dir)"
+
+        echo -e "\n"
+
+}
+if [ ! $file_path ]; then
+        echo "Usage: $0 file_path"
+        exit 1
+fi
+
+if [ -f $file_path ]; then
+        _hexdump
+        _strings
+        _file
+        _exiftool
+        _extract
+else
+        echo "$file_path does not exist"
+fi
